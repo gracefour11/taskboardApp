85087826c3eb5765ff676e3acaf66ec84337912a
gracefour11 <gracecheam@gmail.com>
2022-07-30 16:48:33 +0800

    finish move task functionality

--- capstone/taskboard/templatetags/taskboard_tags.py
+++ capstone/taskboard/templatetags/taskboard_tags.py
@@ -24,3 +24,17 @@ def get_all_users_in_taskboard(boardId):
     users_user2taskboard = User2Taskboard.objects.filter(taskboard=taskboard, delete_ind=DELETE_IND_F).values_list('user', flat=True)
     users = User.objects.filter(id__in=users_user2taskboard)
     return users
+
+@register.simple_tag
+def get_all_sections_in_taskboard(sectionId, boardId):
+    taskboard = Taskboard.objects.get(id=boardId)
+    sections = Section.objects.exclude(id=sectionId).filter(taskboard=taskboard, delete_ind=DELETE_IND_F)
+    return sections
+
+@register.simple_tag
+def get_sections_count_in_taskboard(boardId):
+    taskboard = Taskboard.objects.get(id=boardId)
+    sections_count = Section.objects.filter(taskboard=taskboard, delete_ind=DELETE_IND_F).count()
+    if sections_count > 1:
+        return True
+    return False
