8c6627c2d3d0d2574c6d224be87bb59ac8b8d707
gracefour11 <gracecheam@gmail.com>
2022-08-23 22:21:53 +0800

    register admin models

--- capstone/taskboard/admin.py
+++ capstone/taskboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Taskboard, User2Taskboard, Section, Task
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Taskboard)
+admin.site.register(User2Taskboard)
+admin.site.register(Section)
+admin.site.register(Task)
+
